8fa9ea25bd61fe4c3166a0d679a7d862ddf234e7
Cassiohg <cassiohg@gmail.com>
2016-06-30 02:33:39 -0300

    first commit already working

---
+++ database.js
@@ -0,0 +1,253 @@
+var MongoClient = require('mongodb').MongoClient // mongodb driver for nodejs. installed using npm.
+var ObjectId = require('mongodb').ObjectId
+var assert = require('assert'); // assert module from nodejs default api. will be used inside mongodb callbacks.
+
+
+var Mongodb = function () {
+	this.a = 'lala'
+	this.db
+	this.users
+	this.usersEmailCredentials
+	this.usersFacebookCredentials
+	this.friendShip
+	this.usersRequests
+	this.usersSessions
+};
+
+Mongodb.prototype.connect = function (domainAndPort, databaseName, callback) {
+	var self = this
+	MongoClient.connect('mongodb://'+domainAndPort+'/' + databaseName, function (err, database) {
+		assert.equal(null, err);
+		self.db = database // we will use the database object later.
+		self.users = self.db.collection("users")
+		self.usersEmailCredentials = self.db.collection("emailCredential")
+		self.usersFacebookCredentials = self.db.collection("facebookCredential")
+		self.friendShip = self.db.collection("friendShip")
+		self.usersRequests = self.db.collection("usersRequests")
+		self.usersSessions = self.db.collection("usersSessions")
+		callback()
+	})
+}
+
+
+Mongodb.prototype.existsEmail = function (email, callback) {
+	var self = this
+	self.usersEmailCredentials.find({email: email}, {_id: 0, email: email}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1) callback(true)
+		else callback(false)
+	})
+}
+
+Mongodb.prototype.createAccountWithEmail = function (email, password, displayName, callback) {
+	var self = this
+	self.existsEmail(email, function (exists) {
+		if (exists) {
+			callback()
+		} else {
+			var user = {ct: undefined, city: undefined, pos: undefined, vis: true, n: displayName}
+			self.users.insertOne(user, function (err, result) {
+				var newUserId = result.ops[0]._id
+				var userCredentials = {_id: newUserId, email: email, pw: password}
+				self.usersEmailCredentials.insertOne(userCredentials, function (err, result) {
+					if (err)
+						callback()
+					else
+						callback(newUserId)
+				})
+			})
+		}
+	})
+};
+
+Mongodb.prototype.existsUserId = function (id, callback) {
+	var self = this
+	self.users.find({_id: ObjectId(id)}, {_id: 1}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1) callback(docs[0]._id)
+		else callback()
+	})
+}
+
+Mongodb.prototype.tryLogin = function (email, password, callback) {
+	var self = this
+	self.usersEmailCredentials.find({email: email, pw: password}, {_id: 1}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1) callback(docs[0]._id)
+		else callback()
+	})
+};
+
+Mongodb.prototype.createSessionForId = function (id, source, callback) {
+	var self = this
+	self.usersSessions.find({uid: id, s: source}, {_id: 1}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1)
+			callback(docs[0]._id)
+		else {
+			self.usersSessions.insertOne({uid: id, s: source, d: new Date()}, function (err, result) {
+				callback(result.ops[0]._id) // token is MongoDB ObjectID from sessions collection.
+			})
+		}
+	})
+};
+
+Mongodb.prototype.getUserIdByToken = function (token, callback) {
+	var self = this
+	self.usersSessions.find({_id: ObjectId(token)}, {uid: 1}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1)
+			callback(docs[0].uid)
+		else {
+			callback()
+		}
+	})
+};
+
+
+Mongodb.prototype.deleteSessionForToken = function (token, callback) {
+	var self = this
+	self.usersSessions.remove({uid: ObjectId(token)}, function (err, r) {
+		if (r.result.n > 0) callback(true)
+		else callback(false)
+	})
+};
+
+Mongodb.prototype.getUserById = function (id, callback) {
+	var self = this
+	self.users.find({_id: id}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1) callback(docs[0])
+		else callback()
+	})
+};
+
+Mongodb.prototype.getUserDisplayById = function (id, callback) {
+	var self = this
+	self.users.findOne({_id: id}, {_id: 1, n: 1, pos: 1, city: 1, ct: 1}, function (err, doc) {
+		callback(doc)
+	})
+};
+
+Mongodb.prototype.getUserDisplayByEmail = function (email, callback) {
+	var self = this
+	self.usersEmailCredentials.find({email: email}, {_id: 1}).limit(1).toArray(function (err, docs) {
+		if (docs.length === 1) {
+			self.users.find({_id: docs[0]._id}, {_id: 1, n: 1, pos: 1, city: 1, ct: 1})
+			.limit(1).toArray(function (err, docs) {
+				if (docs.length === 1) callback(docs[0])
+				else callback()
+			})
+		} else {
+			callback()
+		}
+	})
+};
+
+Mongodb.prototype.changeDisplayName = function (id, displayName, callback) {
+	var self = this
+	self.users.updateOne({_id: id}, {$set: {n: displayName}}, function (err, result) {
+		if (err) callback(false)
+		else callback (true)
+	})
+};
+
+Mongodb.prototype.requestFriendship = function (id, friendId, callback) {
+	var self = this
+	self.users.find({_id: friendId}, {_id: 1}).toArray(function (err, docs) {
+		if (docs.length === 1) {
+			friendIsRequestedByUser = {f: friendId, t: id, type: 'friendship', hide: false, d: new Date()}
+			self.usersRequests.insert(friendIsRequestedByUser, function (err, result) {
+				if (err) callback(false)
+				else callback(true)
+			})
+		} else {
+			callback(false)
+		}
+	})
+	
+};
+
+Mongodb.prototype.getUserRequests = function (id, callback) {
+	var self = this
+	self.usersRequests.find({f: id}).toArray(function (err, docs) {
+		var totalOfDocs = docs.length
+		if (totalOfDocs > 0) {
+			var requests = []
+			docs.forEach(function (doc) {
+				self.getUserDisplayById(doc.t, function (user) {
+					if (user !== undefined) {
+						doc.user = user
+						requests.push(doc)
+					}
+					if (--totalOfDocs === 0)
+						callback(requests)
+				})
+			})
+		} else {
+			callback([])
+		}
+	})
+};
+
+Mongodb.prototype.resolveRequest = function (requestId, solution, callback) {
+	var self = this
+	self.usersRequests.findOne({_id: ObjectId(requestId)}, function (err, request) {
+		console.log(request)
+		if (request) {
+			self.usersRequests.remove({_id: request._id}, function (err, r) {
+				if (r.result.n > 0) {	
+					if (request.type === 'friendship') {
+						if (solution === true) {
+							self.addFriendById(request.f, request.t, function (friend) {
+								if (friend) callback(friend)
+								else callback()
+							})
+						} else {
+							callback(false)
+						}
+					}
+				}
+			})
+		} else {
+			callback()
+		}
+	})
+};
+
+Mongodb.prototype.addFriendById = function (id, friendId, callback) {
+	var self = this
+	var userFriendsWithFriend = {_id: {f: id, t: friendId}, d: new Date()}
+	var friendFriendsWithUser = {_id: {f: friendId, t: id}, d: new Date()}
+	self.friendShip.insertMany([userFriendsWithFriend, friendFriendsWithUser], function (err, result) {
+		self.getUserDisplayById(friendId, callback)
+	})
+};
+
+
+Mongodb.prototype.getFriendsDisplay = function (id, callback) {
+	var self = this
+	self.friendShip.find({"_id.f": id}).toArray(function (err, docs) {
+		if (docs.length > 0) {
+			var asyncCounter = docs.length
+			var friends = []
+			docs.forEach(function (obj) {
+				self.getUserDisplayById(obj._id.t, function (user) {
+					if (user !== undefined) {
+						obj.user = user
+						friends.push(obj)
+					}
+					if (--asyncCounter === 0)
+						callback(friends)
+				})
+			})
+		} else {
+			callback([])
+		}
+	})
+};
+
+
+Mongodb.prototype.setFullLocation = function (id, position, city, country, callback) {
+	var self = this
+	self.users.updateOne({_id: id}, {$set: {pos: position, city: city, ct: country}}, function (err, result) {
+		if (err) callback(false)
+		else callback (true)
+	})
+};
+
+module.exports = Mongodb
---
+++ index.js
@@ -0,0 +1,147 @@
+
+var db = new (require(__dirname+"/database.js"))()
+var fs = require('fs')
+var express = require('express');
+var app = express();
+
+app.use(express.static('web'));
+
+app.get('/checkemailexistence/:email', function (req, res) {
+	db.existsEmail(req.params.email, function (id) {
+		if (ok) res.json({msg: "email exists", success: true})
+		else res.json({msg: "email does not exists", success: false})
+	})
+});
+
+app.post('/createaccountemail/:email/:password/:displayname', function (req, res) {
+	db.createAccountWithEmail(req.params.email, req.params.password, req.params.displayname, function (id) {
+		if (id !== undefined) res.jsonp({msg: "account created", success: true})
+		else res.jsonp({msg: "email already exists", success: false, info: req.email})
+	})
+});
+	
+app.get('/loginemail/:email/:password/:source', function (req, res) {
+	db.tryLogin(req.params.email, req.params.password, function (id) {
+		if (id !== undefined) {
+			db.createSessionForId(id, req.params.source, function (token) {
+				db.getUserById(id, function (user) {
+					if (user !== undefined) res.jsonp({msg: "login successful", success: true, user: user, token: token})
+					else res.jsonp({msg: "user credentials exist but user data doesn't exist", success: false})
+				})
+			})
+		} else {
+			res.jsonp({msg: "bad login info", success: false})
+		}
+	})
+});
+
+app.post('/logout/:token', function (req, res) {
+	db.deleteSessionForToken(req.params.token, function (ok) {
+		if (ok) res.jsonp({msg: "logout successful", success: true})
+		else res.jsonp({msg: "already out", success: false})
+	})
+});
+
+var getUserIdByToken = function (res, token, callback) {
+	db.getUserIdByToken(token, function (id) {
+		if (id !== undefined) {
+			callback(id)
+		} else {
+			res.jsonp({msg: "token does not exist", success: false, token: token})	
+		}
+	})
+}
+
+
+app.post('/changename/:token/:displayname', function (req, res) {
+	if (req.params.displayname.length > 0) {
+		getUserIdByToken(res, req.params.token, function (id) {
+			db.changeDisplayName(id, req.params.displayname, function (ok) {
+				if (ok) res.send({msg: "changed display name", success: true})	
+				else res.send({msg: "something bad happened, try again.", success: false})	
+			})
+		})
+	} else {
+		res.send({msg: "bad display name", success: false})	
+	}
+	
+});
+
+app.get('/search/:email/', function (req, res) {
+	db.getUserDisplayByEmail(req.params.email, function (user) {
+		if (user !== undefined) res.send({msg: "found user", success: true, user: user})
+		else res.send({msg: "no user found", success: false})
+	})
+});
+
+app.post('/requestfriendship/:token/:id', function (req, res) {
+	getUserIdByToken(res, req.params.token, function (id) {
+		db.existsUserId(req.params.id, function (friendId) {
+			if (friendId !== undefined) {
+				db.requestFriendship(id, friendId, function (ok) {
+					if (ok) res.send({msg: "friendship requested", success: true})
+					else res.send({msg: "for some reason we couldn't request friendship", success: false})
+				})
+			} else {
+				res.send({msg: "friend doesn't exist", success: false})
+			}
+		})
+	})
+});
+
+app.get('/pendingrequests/:token', function (req, res) {
+	getUserIdByToken(res, req.params.token, function (id) {
+		db.getUserRequests(id, function(requests) {
+			res.send({msg: "all pending requests", success: true, info: requests})
+		})
+	})
+});
+
+app.post('/acceptfriend/:token/:requestId', function (req, res) {
+	getUserIdByToken(res, req.params.token, function (id) {
+		db.resolveRequest(req.params.requestId, true, function (friend) {
+			if (friend !== undefined) res.send({msg: "accepted friend", success: true, user: friend})
+			else res.send({msg: "could not accept friend request", success: false})
+		})
+	})
+});
+
+app.get('/listfriends/:token/', function (req, res) {
+	getUserIdByToken(res, req.params.token, function (id) {
+		db.getFriendsDisplay(id, function (friends) {
+			res.send({msg: "friend's list", success: true, friends: friends})
+		})
+	})
+});
+
+app.post('/location/:token/:lat/:long/:town/:country', function (req, res) {
+	getUserIdByToken(res, req.params.token, function (id) {
+		var town = req.params.town.replace(/%20/g,  ' ')
+		var country = req.params.country.replace(/%20/g,  ' ')
+		db.setFullLocation(id, [req.params.lat, req.params.long], town, country, function (ok) {
+			if (ok) res.send({msg: "updated location", success: true})
+			else res.send({msg: "could not update location", success: false})
+		})
+	})
+});
+
+app.get('/location/:token', function (req, res) {
+	getUserIdByToken(res, req.params.token, function (id) {
+		db.getUserById(id, function (user) {
+			if (user !== undefined) res.send({msg: "found you!", success: true, user: user})
+			else res.send({msg: "could not find you", success: false})
+		})
+	})
+});
+
+
+var configs = JSON.parse(fs.readFileSync(__dirname+"/configs.json"))
+var domainAndPort = configs.database.domainAndPort
+var databaseName = configs.database.databaseName
+db.connect(domainAndPort, databaseName, function () {
+	console.log("-- Connected correctly to mongodb at "+domainAndPort+" to database "+databaseName+".");
+})
+var port = configs.server.port
+app.listen(port, function () {
+	console.log('-- Example app listening on port '+port+'!');
+});
